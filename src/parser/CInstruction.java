aa6ae398e4a065f8eab7806024098c4fbc40ceef
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2025-12-28 13:53:49 +0200

    add basic input validation

--- src/parser/CInstruction.java
+++ src/parser/CInstruction.java
@@ -1,6 +1,12 @@
 package parser;
 
 public class CInstruction implements Instruction {
+  private final String destination;
+
+  public CInstruction(String destination) {
+    this.destination = destination;
+  }
+
   @Override
   public <R> R accept(InstructionVisitor<R> visitor) {
     return visitor.visitInstructionC(this);
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,11 +1,44 @@
 package parser;
 
+import java.util.List;
+
 public class Parser implements IParser {
+  final static private List<String> ALLOWED_DESTINATIONS = List.of(new String[]{
+    "M", "D", "MD", "A", "AM", "AD", "AMD"
+  });
+
   @Override
   public Instruction getInstruction(String line) {
     if (line == null || line.isBlank()) {
       return new AbsentInstruction();
     }
-    return new AInstruction();
+    // remove all whitespaces
+    String strippedLine = line.replaceAll("\s", "");
+    if (strippedLine.startsWith("//")) {
+      return new AbsentInstruction();
+    }
+    if (strippedLine.startsWith("@")) {
+      return new AInstruction();
+    }
+
+    String[] splitDestination = strippedLine.split("=");
+    if (splitDestination.length > 2) {
+      throw new IllegalArgumentException(
+        String.format("the invalid line: expect at most one '=': %s", line)
+      );
+    }
+    String destination = null;
+    if (splitDestination.length == 2) {
+      destination = splitDestination[0];
+      if (!ALLOWED_DESTINATIONS.contains(destination)) {
+        throw new IllegalArgumentException(
+          String.format(
+            "Unexpected destination. Expect one of %s. Got: %s", ALLOWED_DESTINATIONS,
+            line
+          )
+        );
+      }
+    }
+    return new CInstruction(destination);
   }
 }
